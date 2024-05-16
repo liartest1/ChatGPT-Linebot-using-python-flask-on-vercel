65af68950b33278b101106ccab94130c758f5319
liartest1 <jennywen0202@gmail.com>
2024-05-16 09:56:02 +0800

    Update openai.py
    1

--- api/openai.py
+++ api/openai.py
@@ -1,14 +1,7 @@
 from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import openai
 
 app = Flask(__name__)
